e739a915880253d298c784f374dd673f346b6a47
martinvalinoti <martinvalinoti@gmail.com>
2021-01-25 10:26:51 -0300

    proyecto despues de formatear

---
+++ servicios/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+from servicios.models import Servicio
+
+def servicios(request):
+    
+    servicios=Servicio.objects.all()
+    return render(request, "servicios/servicios.html",{"servicios": servicios})
